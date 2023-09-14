8e6adf34c16811e82e61b18c4832d12386a0b3b3
sargoth <elvis.atanasov@gmail.com>
2023-09-14 18:47:14 +0300

    Add files via upload

---
+++ temp.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+while getopts "r" opt; do
+	case $opt in
+		r)
+			echo "Reverting changes made by this script."
+			rm -f /etc/global_spamassassin_enable
+			sed -i 's/globalspamassassin=1/globalspamassassin=0/' /etc/exim.conf.localopts
+			sed -i 's/no_forward_outbound_spam_over_int=5/no_forward_outbound_spam_over_int/' /etc/exim.conf.localopts
+			echo "Changes reverted. Rebuilding and restarting Exim."
+			/scripts/buildeximconf
+			/scripts/restartsrv_exim
+		 	exit 1	
+			;;
+		?)
+			echo "Please use -r to revert changes made by this script."
+			exit 1
+			;;
+	esac
+done
+
+#Check if argument was provided
+#if [ $# -eq 0 ];
+#	then
+#		echo "No command provided."
+#		echo "$0 revert "
+#		exit 1
+#	fi
+
+#provided_argument="$1"
+
+#Define the functionality of the command
+#case "provided_argument" in
+#	"revert")
+#		execute_revert= echo "Reverting changes made by this script."
+#				rm -f /etc/global_spamassassin_enable
+#				sed -i 's/globalspamassassin=1/globalspamassassin=0/' /etc/exim.conf.localopts
+#				sed -i 's/no_forward_outbound_spam_over_int=5/no_forward_outbound_spam_over_int/' /etc/exim.conf.localopts
+#				echo "Changes reverted. Rebuilding and restarting Exim."
+#				/scripts/buildeximconf
+#				/scripts/restartsrv_exim        
+#	;;
+#esac
+
+#Execute revert code
+eval "$execute_revert"
+
+#Check if global_spamassassin_enable exist and if not create it
+if [ -f /etc/global_spamassassin_enable ];
+	then
+		echo "File: /etc/global_spamassassin_enable already exists. Nothing to do"	
+ 	else
+		echo "global_spamassassin_enable file does not exist. Creating it"
+                touch /etc/global_spamassassin_enable
+		echo "File created" 	
+fi
+
+#Check value of globalspamassassin in /etc/exim.conf.localopts and change it to 1 if it is 0
+globalspamassassin_check=$(grep 'globalspamassassin' /etc/exim.conf.localopts)
+localopts_bkp=$(cp -vpf /etc/exim.conf.localopts /etc/exim.conf.localopts-bkp)
+echo "Creating backup of exim.conf.localopts: $localopts_bkp"
+
+if [ "$globalspamassassin_check" == "globalspamassassin=0"  ];
+	then
+		echo "Value for globalspamassassin is 0. Changing to 1"
+		sed -i 's/globalspamassassin=0/globalspamassassin=1/' /etc/exim.conf.localopts
+	       	echo "Value changed to 1"	
+	else
+		echo "Value for globalspamassassin is: $globalspamassassin_check" 
+fi
+
+#Check if no_forward_outbound_spam_over_int has any value set in /etc/exim.conf.localopts and change it to 5
+no_forward_outbound_spam_over_int_check=$(grep 'no_forward_outbound_spam_over_int' /etc/exim.conf.localopts)
+if [ "$no_forward_outbound_spam_over_int_check" == "no_forward_outbound_spam_over_int" ];
+	then
+		echo "no_forward_outbound_spam_over_int does not have any value. Changing it to 5"
+		sed -i 's/no_forward_outbound_spam_over_int/no_forward_outbound_spam_over_int=5/' /etc/exim.conf.localopts
+		echo "Value changed to 5"
+	else
+		echo "Value of no_forward_outbound_spam_over_int is already 5: grep result: $no_forward_outbound_spam_over_int_check. Nothing to do"
+			
+fi
+
+#Rebuild Exim, check if conf is valida and restart service.
+echo "Rebuilding and Restarting Exim"
+exim_restart=$(/scripts/restartsrv_exim | grep 'exim restarted successfully.')
+exim_rebuild=$(/scripts/buildeximconf | grep 'Configuration file passes test!  New configuration file was installed.')
+if [ "$exim_rebuild" == "Configuration file passes test!  New configuration file was installed." ];
+	then
+		echo "Exim rebuilt successfully. Proceeding with restart"
+		echo "$exim_restart"
+	else
+		echo "Exim rebuild failed. Please check server log"
+fi
